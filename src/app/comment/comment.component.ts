3b0bc417919d9b53bd4e51b2d98247ea7922c2a0
Sherin Mathew <jeel@ifourtechnolab.com>
2022-06-15 12:15:48 +0530

    comment section

---
+++ src/app/comment/comment.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent implements OnInit {
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
+
+}
